d17b84a7180734897c435459ace5e66f6c934f42
Jakob Sievers <canned.primat.es@gmail.com>
2013-01-28 21:52:42 +0100

    more deps

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url          "http://github.com/cannedprimates/hog"
   :license      {:name "BSD"
                  :url  "http://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [slingshot           "0.10.3"]])
+  :dependencies [[org.clojure/clojure            "1.4.0"]
+                 [org.clojure/math.combinatorics "0.0.3"]
+                 [org.clojure/math.numeric-tower "0.0.2"]
+                 [slingshot                      "0.10.3"]])
